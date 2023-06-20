3ccb6f128f8599583338cc9e44f047519ff67b92
Mihai Apostol <mihaiapostol153@gmail.com>
2023-06-20 11:00:03 +0300

    Replace path of download folder with images

--- main.py
+++ main.py
@@ -1,24 +1,27 @@
-from icrawler.builtin import GoogleImageCrawler
 import os
-from PIL import Image
 
-dir_name = input('Enter dir name: \n')
+from icrawler.builtin import GoogleImageCrawler
+
 keyword = input('Enter keyword: \n')
 max_num = int(input('Enter max num downloading photo: \n'))
+path_download = os.path.join('C:', 'Users', os.getlogin(), 'Downloads')
+
 
 def download_image():
     # todo: This statement will be to create folder if she was not created
-    if not os.path.exists(dir_name):
-        os.mkdir(dir_name)
+    if not os.path.exists(f'{path_download}/{keyword}'):
+        os.mkdir(f'{path_download}/{keyword}')
 
-    google_crawler = GoogleImageCrawler(storage={'root_dir': f'{dir_name}'})
+    google_crawler = GoogleImageCrawler(storage={'root_dir': f'{path_download}/{keyword}'})
     google_crawler.crawl(keyword=keyword, max_num=max_num)
 
-    if os.path.exists(dir_name):
-        os.listdir(dir_name)
+    if os.path.exists(keyword):
+        os.listdir(keyword)
+
 
 def main():
     download_image()
+
 
 if __name__ == '__main__':
     main()
